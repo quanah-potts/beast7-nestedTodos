1c1a0685d8740e3066cb7bc5fbe762fecd65710b
quanah-potts <39603394+quanah-potts@users.noreply.github.com>
2019-04-20 20:45:14 -0400

    Update beast7-app.js

--- js/beast7-app.js
+++ js/beast7-app.js
@@ -20,7 +20,7 @@ jQuery(function ($) {
 		store: function (namespace, data) {
 			if (arguments.length > 1) {
 				return localStorage.setItem(namespace, JSON.stringify(data));
-      } 
+      			} 
       			else {
 				var store = localStorage.getItem(namespace);
 				return (store && JSON.parse(store)) || [];
